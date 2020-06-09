46e26a5639ee6ef98880284ab3fdecceb856a5d3
papa31 <visidoor@yandex.ru>
2020-06-09 23:49:32 +0500

    Change ports & make .env

--- frontend/lib/apollo.js
+++ frontend/lib/apollo.js
@@ -3,7 +3,7 @@ import { withData } from 'next-apollo'
 
 const config = {
   link: new HttpLink({
-    uri: 'http://localhost:1337/graphql', // Server URL (must be absolute)
+    uri: 'https://secure-sea-74962.herokuapp.com/graphql', // Server URL (must be absolute)
   }),
 }
 export default withData(config)
